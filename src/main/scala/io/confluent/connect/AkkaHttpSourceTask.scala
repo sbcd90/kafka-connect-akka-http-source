df9b1a20c9e2c3d87702150c27180332182e8117
Subhobrata Dey <sbcd90@gmail.com>
2016-07-02 17:12:02 -0400

    implementing poll to test

--- src/main/scala/io/confluent/connect/AkkaHttpSourceTask.scala
+++ src/main/scala/io/confluent/connect/AkkaHttpSourceTask.scala
@@ -1,15 +1,19 @@
 package io.confluent.connect
 
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse}
+import org.apache.kafka.connect.data.Schema
 import org.apache.kafka.connect.source.{SourceRecord, SourceTask}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class AkkaHttpSourceTask extends SourceTask {
+  private val topic = "test8"
+  private val schema = Schema.STRING_SCHEMA
+  private var requestCount: Int = 0
 
   override def start(map: java.util.Map[String, String]): Unit = {
-
+    requestCount += 1
   }
 
   override def stop(): Unit = {
@@ -17,7 +21,19 @@ class AkkaHttpSourceTask extends SourceTask {
   }
 
   override def poll(): java.util.List[SourceRecord] = {
-    null
+    val sourcePartitions = new java.util.HashMap[String, String]()
+    sourcePartitions.put("request", "akka-http")
+
+    val sourceOffsets = new java.util.HashMap[String, String]()
+    sourceOffsets.put("pos", requestCount.toString)
+
+    val record = new SourceRecord(sourcePartitions, sourceOffsets, topic, schema, "Hello World")
+
+    val records = new java.util.ArrayList[SourceRecord]()
+    records.add(record)
+    requestCount += 1
+
+    records
   }
 
   override def version(): String = {
